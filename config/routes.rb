04e1c518980756cd24a7e1d32350b92cf62e6e13
sebaj1993 <seba_j@hotmail.com>
2021-05-09 12:08:43 -0300

    Agrego sessions y route a sidekiqUI para chequear reintentos de peticiones a fedex ante request fallidos a esta

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'StatusPackages/GetTrackingInformationInFedex/', to: 'status_packages#GetTrackingInformationInFedex'
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
+  
 end
